08d686936b5ae20baa85156624678b0b6f1fd4da
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-09 00:02:56 +0530

    Add project files.

---
+++ Count the arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 3, 5, 7, 8, 9, 10 };
+            int count = 0;
+            for(int i =0;i<ar.Length;i++)
+            {
+                count++;
+            }
+            Console.WriteLine("Count the digit of = " + count);
+        }
+    }
+}
